16aa2b7a8020ca5e6098e73a4a276808778f8eed
Your Name <rahul4girgal@gmail.com>
2022-07-06 20:46:41 +0530

    Added : Find Maximum Float From Given Float

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        MaximumData test1 = new MaximumData();
-        Integer[] inputArray = {1,2,3};
-        test1.maxElement(inputArray);
-        Integer[] inputArray1 = {1,3,2};
-        test1.maxElement(inputArray1);
-        Integer[] inputArray2 = {3,2,1};
-        test1.maxElement(inputArray2);
+
+        System.out.println("Maximum Float : ");
+        MaximumData test2 = new MaximumData();
+        Float[] inputArray = {1f,2f,3f};
+        test2.maxElement(inputArray);
+        Float[] inputArray1 = {1f,3f,2f};
+        test2.maxElement(inputArray1);
+        Float[] inputArray2 = {3f,2f,1f};
+        test2.maxElement(inputArray2);
     }
 }
